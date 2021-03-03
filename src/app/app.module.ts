38e4b5f665d77785dcd657af499cbb5d913b20db
Jordan <jordanandrade@outlook.com>
2021-03-03 17:05:16 +0100

    v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { FormulaireComponent } from './formulaire/formulaire.component';
+import { FormsModule } from '@angular/forms';
+import { RecapComponent } from './recap/recap.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FormulaireComponent
+ 
+    FormulaireComponent,
+ 
+    RecapComponent,
+ 
+    HeaderComponent
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
